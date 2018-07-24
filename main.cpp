4c5657b0a27958af24bd26a8ae466cf3ae6c426a
leek <13624602024@163.com>
2018-07-24 23:18:58 +0800

    2

--- main.cpp
+++ main.cpp
@@ -8,11 +8,14 @@ extern int gpio_get_value(unsigned int gpio);
 
 static void gpio_test()
 {
-    int led = IMX_GPIO_NR(1,25);
-    gpio_export(led);
-    gpio_set_direction(led, 1);
-    gpio_set_value(led, 1);
-    gpio_get_value(led);
+    //获取用户输入，如果为ESC，则退出该函数
+    while(1) {
+        int led = IMX_GPIO_NR(1,25);
+        gpio_export(led);
+        gpio_set_direction(led, 1);
+        gpio_set_value(led, 1);
+        gpio_get_value(led);
+    }
 }
 
 
@@ -38,11 +41,47 @@ static void ethernet_test()
 
 
 }
-int main(int argc, char *argv[])
-{
-    int i;
-    /*测试菜单*/
 
-    printf("hello word!\n");
-    return 0;
+void Temp_function()
+{
+    printf("\nPlease input 1-11 to select test!!!\n");
+}
+
+
+struct {
+    void (*fun)(void);
+    char *tip;
+}CmdTip[] = {
+                { Temp_function, "Please input 1-11 to select test" } ,
+                { gpio_test, "Please input 1-11 to select test" } ,
+                { 0, 0}
+            };
+
+/******************************************************************************
+* @brief:Main
+* @author:leek
+* @date 2018/07/24
+*******************************************************************************/
+int Main(void)
+{
+    int i, idx;
+
+    printf("<***************************************>\n");
+    printf("               iMX6 Test Program\n");
+    printf("<***************************************>\n");
+
+    /*测试菜单*/
+    while(1)
+    {
+        printf("\nPlease select function : \n");
+        for(i=0; CmdTip[i].fun!=0; i++)
+            printf("%d : %s\n", i, CmdTip[i].tip);
+
+        scanf("%s", idx) ;
+        if(idx<i)
+        {
+            (*CmdTip[idx].fun)();
+            sleep(20);
+        }
+    }
 }
